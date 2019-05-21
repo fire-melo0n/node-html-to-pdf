7c285dc5e7588690d48d6bf397e0549e551e3193
Jamie Hall <jamie@jamie-hall.me>
2019-05-21 18:55:14 +0700

    Now generates PDF’s

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,28 @@
-declare class HTMLToPDF {
+import { PDFOptions } from 'puppeteer';
 
-  
+export default class HTMLToPDF {
+
+  constructor(
+    params: {
+      templatePath: string,
+      data: object,
+      options?: {
+        puppeteerPDFOptions: PDFOptions
+      }
+    }
+  );
+
+  getPDF(): Promise<string|Buffer>;
+
+  private _getTemplate(filePath: string): Promise<string>;
+
+  private _parseTemplate(
+    template: string,
+    data: object
+  ): Promise<string>;
+
+  private _saveHTML(html: string): Promise<string>;
+
+  private _createPDF(htmlPath: string): Promise<Buffer>;
 
 }
-
-export = HTMLToPDF;
